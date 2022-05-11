9c45fdca88b7368503dc8aa2c01365272af9f405
Rik Harink <Rik.Harink@topicus.nl>
2022-05-11 10:51:12 +0200

    eslint + prettier

--- postbuild.mjs
+++ postbuild.mjs
@@ -1,30 +1,30 @@
-import { join } from "path";
-import chalk from "chalk";
-import fs from "fs";
-import archiver from "archiver";
-import inliner from "web-resource-inliner";
+import { join } from 'path';
+import chalk from 'chalk';
+import fs from 'fs';
+import archiver from 'archiver';
+import inliner from 'web-resource-inliner';
 const { html } = inliner;
-import util from "util";
-import { minify as htmlMinify } from "html-minifier";
-import ect from "ect-bin";
-import { execFileSync } from "child_process";
+import util from 'util';
+import { minify as htmlMinify } from 'html-minifier';
+import ect from 'ect-bin';
+import { execFileSync } from 'child_process';
 
 const inlineHtml = util.promisify(html);
 
 function getHtml() {
-  return fs.readFileSync(join("dist", "index.html"), "utf8");
+  return fs.readFileSync(join('dist', 'index.html'), 'utf8');
 }
 
 async function inline(html) {
-  console.log(chalk.blue("Inlining..."));
+  console.log(chalk.blue('Inlining...'));
   return await inlineHtml({
     fileContent: html,
-    relativeTo: "dist",
+    relativeTo: 'dist',
   });
 }
 
 function minify(html) {
-  console.log(chalk.blue("Minifying..."));
+  console.log(chalk.blue('Minifying...'));
   return htmlMinify(html, {
     collapseWhitespace: true,
     removeAttributeQuotes: true,
@@ -47,20 +47,25 @@ function minify(html) {
 
 function archive(html) {
   return new Promise(async (resolve, reject) => {
-    console.log(chalk.blue("Zipping..."));
-    const outputPath = join("dist", "game.zip");
+    console.log(chalk.blue('Zipping...'));
+    const outputPath = join('dist', 'game.zip');
     let output = fs.createWriteStream(outputPath);
-    let archive = archiver("zip");
+    let archive = archiver('zip');
     const max = 13 * 1024;
-  
-    output.on("close", function () {
-      console.log(chalk.blue("ECT optimizing..."));
-      const result = execFileSync(ect, ["-100500", "-strip", "-zip", outputPath]);
-      console.log(chalk.yellow(result.toString("utf8")));
-  
+
+    output.on('close', function () {
+      console.log(chalk.blue('ECT optimizing...'));
+      const result = execFileSync(ect, [
+        '-100500',
+        '-strip',
+        '-zip',
+        outputPath,
+      ]);
+      console.log(chalk.yellow(result.toString('utf8')));
+
       const stats = fs.statSync(outputPath);
-  
-      const bytes = stats["size"];
+
+      const bytes = stats['size'];
       const percent = ((bytes / max) * 100).toFixed(2);
       if (bytes > max) {
         console.error(chalk.red(`Size overflow: ${bytes} bytes (${percent}%)`));
@@ -69,27 +74,27 @@ function archive(html) {
       }
       resolve();
     });
-  
-    archive.on("warning", function (err) {
-      if (err.code === "ENOENT") {
+
+    archive.on('warning', function (err) {
+      if (err.code === 'ENOENT') {
         console.warn(err);
       } else {
         reject(err);
       }
     });
-  
-    archive.on("error", function (err) {
+
+    archive.on('error', function (err) {
       reject(err);
     });
-  
+
     archive.pipe(output);
-    archive.append(html, { name: "index.html" });
+    archive.append(html, { name: 'index.html' });
     await archive.finalize();
-  })
+  });
 }
 
 let result = getHtml();
 result = await inline(result);
 result = minify(result);
-fs.writeFileSync(join("dist", "index.min.html"), result);
+fs.writeFileSync(join('dist', 'index.min.html'), result);
 await archive(result);
--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,26 +1,26 @@
-import { join } from "path";
-import { defineConfig } from "rollup";
-import { terser } from "rollup-plugin-terser";
-import typescript from "@rollup/plugin-typescript";
-import json from "@rollup/plugin-json";
-import image from "@rollup/plugin-image";
-import replace from "@rollup/plugin-replace";
-import html2 from "rollup-plugin-html2";
-import { Packer } from "roadroller";
-import livereload from "rollup-plugin-livereload";
-import serve from "rollup-plugin-serve";
+import { join } from 'path';
+import { defineConfig } from 'rollup';
+import { terser } from 'rollup-plugin-terser';
+import typescript from '@rollup/plugin-typescript';
+import json from '@rollup/plugin-json';
+import image from '@rollup/plugin-image';
+import replace from '@rollup/plugin-replace';
+import html2 from 'rollup-plugin-html2';
+import { Packer } from 'roadroller';
+import livereload from 'rollup-plugin-livereload';
+import serve from 'rollup-plugin-serve';
 
-const env = process.env.NODE_ENV || "development";
-const isDev = env === "development";
-const isProduction = env === "production";
+const env = process.env.NODE_ENV || 'development';
+const isDev = env === 'development';
+const isProduction = env === 'production';
 
 const roadroller = {
   renderChunk(data) {
     const inputs = [
       {
         data,
-        type: "js",
-        action: "eval",
+        type: 'js',
+        action: 'eval',
       },
     ];
 
@@ -37,12 +37,15 @@ const roadroller = {
 
 const plugins = [
   replace({
-    "process.env.NODE_ENV": JSON.stringify(env),
     preventAssignment: true,
+    values: {
+      'process.env.NODE_ENV': JSON.stringify(env),
+      'process.env.DEBUG': JSON.stringify(isDev),
+    },
   }),
   html2({
-    title: "JS13K 2022",
-    template: join("src", "index.html"),
+    title: 'JS13K 2022',
+    template: join('src', 'index.html'),
     minify: {
       removeComments: !isDev,
       collapseWhitespace: !isDev,
@@ -59,7 +62,7 @@ const plugins = [
     compress: {
       keep_fargs: false,
       passes: 10,
-      pure_funcs: ["assert", "debug"],
+      pure_funcs: ['assert', 'debug'],
       pure_getters: true,
       unsafe: true,
       unsafe_arrows: true,
@@ -80,15 +83,19 @@ const plugins = [
     },
   }),
   isProduction && roadroller,
-  isDev && livereload({ watch: "dist" }),
-  isDev && serve("dist"),
+  isDev && livereload({ watch: 'dist' }),
+  isDev &&
+    serve({
+      open: true,
+      contentBase: 'dist',
+    }),
 ];
 
 export default defineConfig({
-  input: join("src", "index.ts"),
+  input: join('src', 'index.ts'),
   output: {
-    file: join("dist", "bundle.js"),
-    format: "iife",
+    file: join('dist', 'bundle.js'),
+    format: 'iife',
     sourcemap: isDev,
     strict: false,
   },
